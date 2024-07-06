27e97070757b2fe8040471e808d4a6f020903b0f
PedroPampolini <pedromendicino25@gmail.com>
2024-07-05 22:31:33 -0300

    closes #2

--- backend/NumberCollection/models/users.py
+++ backend/NumberCollection/models/users.py
@@ -1,15 +1,21 @@
 from django.db import models
 from NumberCollection.models.model import BasicModel
+from bcrypt import hashpw, gensalt
+
 
 class User(BasicModel):
     class Meta:
         db_table = "users"
 
     username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
+    password = models.CharField(max_length=60)
     email = models.EmailField(max_length=50)
     phone = models.CharField(max_length=50)
     address = models.CharField(max_length=100)
+
+    def save(self, *args, **kwargs):
+        self.password = hashpw(self.password.encode(), gensalt()).decode()
+        super(User, self).save(*args, **kwargs)
     
     def __str__(self):
         return self.username
